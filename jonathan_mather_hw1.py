90bd9478cdfeefbca9499d21125d33597a93372a
Jonathan Mather <matherj@flip3.engr.oregonstate.edu>
2021-04-25 23:38:48 -0700

    Added Error Handling

--- jonathan_mather_hw1.py
+++ jonathan_mather_hw1.py
@@ -1,5 +1,12 @@
 in_year = int(input("Please enter a year to check if it is a leap year: ")
 
+	while True:
+		try:
+			year = int(in_year)
+			break
+		except valueError:
+			print("Input must be an integer")
+
 if(in_year % 4) == 0:
 	if(in_year % 100) == 0:
 		if(in_year % 400) == 0:
